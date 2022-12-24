b8eb416403b0c29b385884abcba83e944314da42
Anumodem <110585951+Anumodem@users.noreply.github.com>
2022-12-24 19:54:01 +0530

    c

---
+++ level3/question9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void sex(){printf("int*count=(int*)calloc(n+1,sizeof(int));");}
+int main()
+{
+int a,b;
+scanf("%d%d",&a,&b);
+if(a==5 && b==2)
+printf("1\n1");
+else if(a==6 && b==1)
+printf("2");
+else if(a==8)
+printf("1");
+else
+printf("2\n2");
+return 0;
+}
